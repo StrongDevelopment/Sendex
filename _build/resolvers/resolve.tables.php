ac6472331ae6cb1b48c408fb85686ced54306cbc
Oleg <admin@strongdev.net>
2016-10-21 07:26:39 +0300

    model

--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -7,6 +7,22 @@ if ($transport->xpdo) {
 
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
+            $modelPath = $modx->getOption('sendex_core_path', null, $modx->getOption('core_path').'components/sendex').'model';
+            $modx->addPackage('sendex', $modelPath);
+
+            $manager = $modx->getManager();
+            $objects = array(
+                'sxNewsletter',
+                'sxSubscriber',
+                'sxQueue',
+            );
+
+            foreach ($objects as $object) {
+                $manager->createObjectContainer($objects);
+            }
+
+            break;
+
         case xPDOTransport::ACTION_UPGRADE:
             $modelPath = $modx->getOption('sendex_core_path', null,
                     $modx->getOption('core_path') . 'components/sendex/') . 'model/';
